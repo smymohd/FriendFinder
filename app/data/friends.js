4379682413d50c0d302a33b69b2b3b3cc1dee712
user <edwardhartman@macs-iMac-98.local>
2018-06-28 07:28:46 -0500

    yes

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,25 +7,26 @@
 
 var friendArray = [
     {
-      "name":"Angelina Jolie",
-      "photo":"https://upload.wikimedia.org/wikipedia/commons/3/34/Angelina_Jolie_at_Davos_crop.jpg",
-      "scores":[
-          5,
-          1,
-          4,
-          4,
-          5,
-          1,
-          2,
-          5,
-          4,
-          1
+        "name": "Angelina Jolie",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/3/34/Angelina_Jolie_at_Davos_crop.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+
         ]
     },
     {
-        "name":"Justin Timberlake",
-        "photo":"https://upload.wikimedia.org/wikipedia/commons/3/3a/Justin_Timberlake_2007-2.jpg",
-        "scores":[
+        "name" : "Justin Timberlake",
+        "photo" : "https://upload.wikimedia.org/wikipedia/commons/3/3a/Justin_Timberlake_2007-2.jpg",
+        "scores": [
             2,
             5,
             1,
@@ -36,12 +37,12 @@ var friendArray = [
             4,
             3,
             4
-          ]
-      },
-      {
-        "name":"Cardi B",
-        "photo":"https://en.wikipedia.org/wiki/Cardi_B#/media/File:Cardi_B_2016.jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name" : "Cardi B",
+        "photo": "https://en.wikipedia.org/wiki/Cardi_B#/media/File:Cardi_B_2016.jpg",
+        "scores" : [
             4,
             2,
             1,
@@ -52,12 +53,12 @@ var friendArray = [
             3,
             4,
             5
-          ]
-      },
-      {
-        "name":"LeBron James",
-        "photo":"https://en.wikipedia.org/wiki/LeBron_James#/media/File:LeBron_James_going_for_slamdunk.jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name" : "LeBron James",
+        "photo" : "https://en.wikipedia.org/wiki/LeBron_James#/media/File:LeBron_James_going_for_slamdunk.jpg",
+        "scores" : [
             1,
             2,
             3,
@@ -68,12 +69,12 @@ var friendArray = [
             5,
             5,
             3
-          ]
-      },
-      {
-        "name":"Denzel Washington",
-        "photo":"https://en.wikipedia.org/wiki/Denzel_Washington#/media/File:The_Equalizer_07_(15127104638).jpg",
-        "scores":[
+        ]
+    },
+    {
+        "name": "Denzel Washington",
+        "photo" : "https://en.wikipedia.org/wiki/Denzel_Washington#/media/File:The_Equalizer_07_(15127104638).jpg",
+        "scores" : [
             4,
             3,
             2,
@@ -84,17 +85,9 @@ var friendArray = [
             2,
             4,
             1
-          ]
-      },
+        ]
+    },
+];
+module.expoers = friends;
 
 
-
-
-
-  
-    ];
-    
-    
-    // Note how we export the array. This makes it accessible to other files using require.
-    module.exports = friends;
-    
--- server.js
+++ server.js
@@ -6,22 +6,18 @@ var PORT = process.env.PORT || 8080;
 
 
 
-// create application/x-www-form-urlencoded parser
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-// parse various different cutsom JSON types as JSON
-app.use(bodyParser.json({type: 'application/**+json'}))
+app.use(bodyParser.json({type: 'application/***json'}));
 
-// parse various custom thing into a Buffer
-app.use(bodyParser.raw({type: 'application/vnd.custom-type'}))
+app.use(bodyParser.raw({type: 'application/vnd.custom-type'}));
 
-// parse an HTML body into a string
-app.use(bodyParser.text({type: 'text/html'}))
+app.use(bodyParser.text({type: 'text/html'}));
 
 require("./routing/apiRoutes.js")(app);
 require("./routing/htmlRoutes.js")(app);
-
-
 app.listen(PORT, function(){
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT: "  + PORT);
 });
+
